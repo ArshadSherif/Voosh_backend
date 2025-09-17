10307da39f3dacf81db454908ba763ec484e08d4
Arshad <90891115+ArshadSherif@users.noreply.github.com.>
2025-09-18 01:14:28 +0530

    added __main__

--- main.py
+++ main.py
@@ -115,3 +115,7 @@ def list_sessions():
 
 
 
+if __name__ == "__main__":
+    import os, uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
