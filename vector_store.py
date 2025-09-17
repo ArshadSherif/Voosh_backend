cf0e8719496eade74863358635174116717700e8
Arshad <90891115+ArshadSherif@users.noreply.github.com.>
2025-09-17 21:10:38 +0530

    smaller model

--- vector_store.py
+++ vector_store.py
@@ -8,7 +8,7 @@ from redis.commands.search.query import Query
 
 #config
 EMBED_DIM = 384
-model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
+model = SentenceTransformer("paraphrase-albert-small-v2")
 
 
 def get_embedding(text:str):
